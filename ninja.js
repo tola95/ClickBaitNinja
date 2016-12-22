12b4ec81172d5863f339581611c74b3e3a6df518
Tola Babasola <OmotolaBabasola1@Omotola-Babasolas-MacBook-Pro.local>
2016-12-22 15:31:25 +0100

    Added overlay div to project. Overlay currently does not display iframe

--- ninja.js
+++ ninja.js
@@ -1,9 +1,29 @@
 console.log('ninja online');
-$('a').hover(function()
-{
+
+$.get(chrome.extension.getURL('/overlay.html'), function(data) {
+    //$(data).appendTo('body');
+    // Or if you're using jQuery 1.8+:
+    $($.parseHTML(data)).appendTo('body');
+});
+
+$(document).ready(function(){
+    $("#overlay").hide();
+});
+
+$('a').hover(function(e)
+{  
+    var x = e.pageX;
+    var y = e.pageY;
     $(this).data('timeout', window.setTimeout(function()
     {
         console.log('hovered for 2 seconds');
+        e.preventDefault();
+        $("#overlay iframe").attr("src", $(this).attr('href'));
+
+        $('#overlay').css('left',e.pageX); 
+        $('#overlay').css('top',e.pageY);
+        $('#overlay').css('display','inline');
+        $("#overlay").css("position", "absolute"); 
     }, 2000));
 },
 function()
